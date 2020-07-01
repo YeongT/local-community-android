073982aa170d87a47a125f215f8ec4029584ed6a
YeongChang Yun <45313479+YunYeongChang@users.noreply.github.com>
2020-07-01 20:57:56 +0900

    Initial commit

---
+++ app/src/main/java/com/implude/localcommunity/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.implude.localcommunity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
