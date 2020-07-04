be7ec8b7719ded54e67d72d26196d193f5fe6f82
Kimseongho <kshk1743@naver.com>
2020-07-04 21:11:34 +0900

    update : SignupActivity.kt , activity_sign Add : UserRegisterModel , Api.kt

--- app/src/main/java/com/implude/localcommunity/SignUpActivity.kt
+++ app/src/main/java/com/implude/localcommunity/SignUpActivity.kt
@@ -1,15 +1,26 @@
 package com.implude.localcommunity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.implude.localcommunity.models.UserRegisterModel
 import kotlinx.android.synthetic.main.activity_sign.*
-import java.net.HttpURLConnection
-import java.net.URL
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.regex.Pattern
-import javax.net.ssl.HttpsURLConnection
+
+const val BASE_URL = "https://api.hakbong.me/"
 
 class SignUpActivity : AppCompatActivity() {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,20 +29,57 @@ class SignUpActivity : AppCompatActivity() {
 
         enroll_btn.setOnClickListener {
             val email = signup_email.text.toString()
-            val passwrd1 = signup_pwd.text.toString()
-            val passwrd2 = signup_pwd2.text.toString()
+            val password1 = signup_pwd.text.toString()
+            val password2 = signup_pwd2.text.toString()
             val nickname = signup_name.text.toString()
 
-            if (!Pattern.matches("^[_a-zA-Z0-9-\\.]+@[\\.a-zA-Z0-9-]+\\.[a-zA-Z]+\$", email)) // 이메일
-                Log.e("email", "이메일 형식이 아닙니다.")
+//            if (!Pattern.matches("^[_a-zA-Z0-9-\\.]+@[\\.a-zA-Z0-9-]+\\.[a-zA-Z]+\$", email)) // 이메일
+//                Log.e("email", "이메일 형식이 아닙니다.")
+//
+//
+//            if (!Pattern.matches("^[a-zA-Z0-9!@#$^*&()]     {8,20}\$", password1)) // 비밀번호
+//                Log.e("passward chk", "비밀번호 형식이 아닙니다.")
+//
+//            if (!password1.equals(password2))
+//                Log.e("passward chk", "비밀번호와 비밀번호 확인이 동일하지 않습니다.")
 
 
-            if (!Pattern.matches("^[a-zA-Z0-9!@#$^*&()]     {8,20}\$", passwrd1)) // 비밀번호
-                Log.e("passward chk", "비밀번호 형식이 아닙니다.")
+            val model = UserRegisterModel(
+                email,
+                password1,
+                nickname,
+                "010-1234-5678",
+                "1",
+                "{\"state\":\"경기도\",\"city\":\"안산시\",\"dong\":\"와동\"}\n"
+            )
 
+            val api = retrofit.create(Api::class.java)
+            api.signUp(model).enqueue(object : Callback<String> {
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    Log.e("APITEST_FAIL",t.message.toString())
+                    t.printStackTrace()
 
-            Log.e("ID chk", email + " " + passwrd1 + " " + passwrd2 + " " + nickname)
+                    Toast.makeText(this@SignUpActivity,"오류",Toast.LENGTH_LONG).show()
 
+                }
+
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    Log.e("APITEST-SUC", response.code().toString())
+
+                    if(response.code().toString() != "200") {
+                        val toast = Toast.makeText(this@SignUpActivity, "오류", Toast.LENGTH_LONG)
+                        toast.setGravity(
+                            Gravity.CENTER,
+                            Gravity.CENTER_HORIZONTAL,
+                            Gravity.CENTER_VERTICAL
+                        )
+
+                        toast.show()
+                    }
+                }
+
+            })
         }
     }
+
 }
