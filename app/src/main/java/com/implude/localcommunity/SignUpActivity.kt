4421f51d4f164093f800a0c73bc9844fbb8a0b6c
Kimseongho <kshk1743@naver.com>
2020-08-05 21:38:04 +0900

    add : place picker

--- app/src/main/java/com/implude/localcommunity/SignUpActivity.kt
+++ app/src/main/java/com/implude/localcommunity/SignUpActivity.kt
@@ -1,10 +1,17 @@
 package com.implude.localcommunity
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.implude.localcommunity.models.UserRegisterModel
 import kotlinx.android.synthetic.main.activity_sign.*
 import retrofit2.Call
@@ -15,7 +22,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.regex.Pattern
 
 const val BASE_URL = "https://api.hakbong.me/"
-
+private val AUTOCOMPLETE_REQUEST_CODE = 1
 class SignUpActivity : AppCompatActivity() {
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
@@ -27,28 +34,38 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
 
+        signup_area.setOnClickListener{
+
+
+            Places.initialize(applicationContext, "AIzaSyAoHiDeQI7Br0T6aWmXbXG9Vb259PrSucM")
+
+
+            val placesClient = Places.createClient(this)
+
+            val fields = listOf(Place.Field.ADDRESS)
+
+
+            val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
+                .build(this)
+            startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
+
+        }
         enroll_btn.setOnClickListener {
             val email = signup_email.text.toString()
             val password1 = signup_pwd.text.toString()
 
             val nickname = signup_name.text.toString()
 
-//            if (!Pattern.matches("^[_a-zA-Z0-9-\\.]+@[\\.a-zA-Z0-9-]+\\.[a-zA-Z]+\$", email)) // 이메일
-//                Log.e("email", "이메일 형식이 아닙니다.")
+            val phonenum = signup_phonenum.text.toString()
+
 //
-//
-//            if (!Pattern.matches("^[a-zA-Z0-9!@#$^*&()]     {8,20}\$", password1)) // 비밀번호
-//                Log.e("passward chk", "비밀번호 형식이 아닙니다.")
-//
-//            if (!password1.equals(password2))
-//                Log.e("passward chk", "비밀번호와 비밀번호 확인이 동일하지 않습니다.")
 
 
             val model = UserRegisterModel(
                 email,
                 password1,
                 nickname,
-                "010-1234-5678",
+                phonenum,
                 "1",
                 "{\"state\":\"경기도\",\"city\":\"안산시\",\"dong\":\"와동\"}\n"
             )
@@ -82,4 +99,30 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    data?.let {
+                        val place = Autocomplete.getPlaceFromIntent(data)
+                        Log.i("place",place.toString())
+
+
+                    }
+                }
+                AutocompleteActivity.RESULT_ERROR -> {
+                    data?.let {
+                        val status = Autocomplete.getStatusFromIntent(data)
+                        Toast.makeText(this, getString(R.string.Sign_err_location)+status.toString(),Toast.LENGTH_LONG).show()
+                    }
+                }
+                Activity.RESULT_CANCELED -> {
+                    // The user canceled the operation.
+                }
+            }
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 }
