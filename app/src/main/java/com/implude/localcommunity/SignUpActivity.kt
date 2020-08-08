d6dc434f45eff9eb339af012039fa480f40ef453
Kimseongho <kshk1743@naver.com>
2020-08-08 21:51:42 +0900

    feat : 회원가입 페이지 완성

--- app/src/main/java/com/implude/localcommunity/SignUpActivity.kt
+++ app/src/main/java/com/implude/localcommunity/SignUpActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.Gravity
 import android.view.View
@@ -51,7 +53,7 @@ class SignUpActivity : AppCompatActivity() {
                 this,
                 getString(R.string.Sign_err_gender),
                 Toast.LENGTH_SHORT
-            ).show();
+            ).show()
 
             val email = signup_email.text.toString()
             val password1 = signup_pwd.text.toString()
@@ -60,6 +62,32 @@ class SignUpActivity : AppCompatActivity() {
             var gender = if (signup_chk_male.isChecked) 1 else 2
 
 
+
+            if (!Pattern.matches(
+                    "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}\$",
+                    email
+                )
+            ) {
+                Toast.makeText(this, "올바른 이매일이 아닙니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (!Pattern.matches(
+                    "^.*(?=^.{8,15}\$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#\$%^&+=]).*\$",
+                    password1
+                )
+            ) {
+                Toast.makeText(this, "올바른 비밀번호가 아닙니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (!Pattern.matches(
+                    "^(?:(010-?\\d{4})|(01[1|6|7|8|9]-?\\d{3,4}))-?\\d{4}\$",
+                    phonenum
+                )
+            ) {
+                Toast.makeText(this, "올바른 전화번호가 아닙니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
             val model = UserRegisterModel(
                 email,
                 password1,
@@ -94,6 +122,119 @@ class SignUpActivity : AppCompatActivity() {
 
             })
         }
+
+
+
+        signup_chk_female.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (signup_chk_female.isChecked) {
+                signup_chk_male.isChecked = false
+            }
+        }
+        signup_chk_male.setOnCheckedChangeListener { butonView, isChecked ->
+            if (signup_chk_male.isChecked) {
+                signup_chk_female.isChecked = false
+            }
+
+        }
+
+        signup_email.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+                if (!Pattern.matches(
+                        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                                "\\@" +
+                                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                                "(" +
+                                "\\." +
+                                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                                ")+",
+                        signup_email.text
+                    )
+                ) {
+                    signup_email_text.text = "이메일이 올바르지 않습니다"
+                    signup_email_text.setTextColor(getColor(R.color.incorrect))
+                } else {
+                    signup_email_text.text = "올바른 이메일 입니다"
+                    signup_email_text.setTextColor(getColor(R.color.correct))
+
+                }
+
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+
+            }
+
+
+        })
+
+        signup_pwd.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+                if (!Pattern.matches(
+                        "^.*(?=^.{8,15}\$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#\$%^&+=]).*\$",
+                        signup_pwd.text
+                    )
+                ) {
+                    signup_pwd_text.text = "비밀번호가 올바르지 않습니다"
+                    signup_pwd_text.setTextColor(getColor(R.color.incorrect))
+                } else {
+                    signup_pwd_text.text = "올바른 비밀번호 입니다"
+                    signup_pwd_text.setTextColor(getColor(R.color.correct))
+
+                }
+
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+
+            }
+
+
+        })
+
+        signup_phonenum.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+                if (!Pattern.matches(
+                        "^(?:(010-?\\d{4})|(01[1|6|7|8|9]-?\\d{3,4}))-?\\d{4}\$",
+                        signup_phonenum.text
+                    )
+                ) {
+                    signup_phonenum_text.text = "전화번호가 올바르지 않습니다"
+                    signup_phonenum_text.setTextColor(getColor(R.color.incorrect))
+                } else {
+                    signup_phonenum_text.text = "올바른 전화번호 입니다"
+                    signup_phonenum_text.setTextColor(getColor(R.color.correct))
+
+                }
+
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+
+            }
+
+
+        })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -104,7 +245,7 @@ class SignUpActivity : AppCompatActivity() {
                 Activity.RESULT_OK -> {
                     data?.let {
                         place = Autocomplete.getPlaceFromIntent(data).address.toString()
-                        signup_area.setText(place)
+                        signup_area.text = place
                     }
                 }
                 AutocompleteActivity.RESULT_ERROR -> {
--- app/src/main/java/com/implude/localcommunity/models/UserRegisterModel.kt
+++ app/src/main/java/com/implude/localcommunity/models/UserRegisterModel.kt
@@ -6,5 +6,5 @@ data class UserRegisterModel(
     val name: String,
     val phone: String,
     val gender: Number,
-    val area: String
+    val areaString: String
 )
