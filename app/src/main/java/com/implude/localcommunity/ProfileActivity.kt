ca390d7bca57d80c94d285ecd24ae8f733f0cb8c
KimJaeWoo <kjw030820@naver.com>
2020-09-01 16:06:45 +0900

    git : merge test

---
+++ app/src/main/java/com/implude/localcommunity/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.implude.localcommunity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
