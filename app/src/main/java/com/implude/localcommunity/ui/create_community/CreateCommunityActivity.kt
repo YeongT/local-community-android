05542e34ac84c9f9df0ab0a562337385d40d5fb0
KimJaeWoo <kjw030820@naver.com>
2020-11-11 22:29:29 +0900

    feat : change activity_create_community.xml

---
+++ app/src/main/java/com/implude/localcommunity/ui/create_community/CreateCommunityActivity.kt
@@ -0,0 +1,12 @@
+package com.implude.localcommunity.ui.create_community
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.implude.localcommunity.R
+
+class CreateCommunityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_community)
+    }
+}
--- app/src/main/java/com/implude/localcommunity/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/implude/localcommunity/ui/splash/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import com.implude.localcommunity.ui.create_community.CreateCommunityActivity
 import com.implude.localcommunity.ui.link.LinkActivity
 import com.implude.localcommunity.ui.main.MainActivity
 
@@ -11,7 +12,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(@NonNull savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, CreateCommunityActivity::class.java)
         startActivity(intent)
         finish()
     }
