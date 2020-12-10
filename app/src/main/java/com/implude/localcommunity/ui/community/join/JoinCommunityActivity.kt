46c285181247b8631d1fca4e58bc8bd5d1c8f79f
JaeWoo <49425325+Kim-JaeWoo@users.noreply.github.com>
2020-12-10 09:31:01 +0900

    feat : add JoinCommunityActivity (#25)
    * feat : add fragment with MainActivity
    
    * feat : add JoinCommunityActivity
    
    Co-authored-by: YeongChang Yun <45313479+YunYeongChang@users.noreply.github.com>

---
+++ app/src/main/java/com/implude/localcommunity/ui/community/join/JoinCommunityActivity.kt
@@ -0,0 +1,12 @@
+package com.implude.localcommunity.ui.community.join
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.implude.localcommunity.R
+
+class JoinCommunityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_community)
+    }
+}
